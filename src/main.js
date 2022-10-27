ba2471bd525c1dcfb5cfb24e65be112794457a74
Alberto <betim009@gmail.com>
2022-10-27 19:38:09 -0300

    two functions

--- src/main.js
+++ src/main.js
@@ -1,27 +1,81 @@
+// const ninjaUl = document.getElementById('ninjas')
+// const handleFetch = async () => {
+//     const url = 'https://naruto-api.herokuapp.com/api/v1/characters';
+//     const response = await fetch(url);
+//     // console.log(response);
+//     const data = response.json();
+    
+//     // console.log(data);
+    
+//     data
+//         .then((ninjas) => {
+//             const listNinjas = ninjas.map(({name, id, images}) => ({
+//                 name,
+//                 id,
+//                 images
+//             }));
+//             // console.log(listNinjas[0].images[0]);
+
+//             handleNames(listNinjas);
+
+//         })
+    
+    
+    
+// }
+
+// const handleNames = (ninja) => {
+//     const render = ninja.map(({name, id, images}) => 
+//     `<li>${id}º ninja: ${name}</li>
+//     <img class="card-image" src="${images[0]}"/> 
+//     `).join('')
+
+//     ninjaUl.innerHTML = render;
+// }
+
+
+// handleFetch();
+
+// ---------------------- Divisão. dos métodos
+
+const url = 'https://naruto-api.herokuapp.com/api/v1/characters';
 const ninjaUl = document.getElementById('ninjas')
-const handleFetch = async () => {
-    const url = 'https://naruto-api.herokuapp.com/api/v1/characters';
-    const response = await fetch(url);
-    const data = response.json();
 
-    data
-        .then((ninjas) => {
-            const ninjasNome = ninjas.map(({name, id}) => ({name, id}));
-            // console.log(ninjasNome);
-
-            handleNames(ninjasNome);
-
-        })
-    
-    
-    
+const validate = ninjaData => {
+    console.log(ninjaData)
+if (!ninjaData.ok) {
+        throw new Error(`HTTP error, status ${ninjaData.status}`)
+    }
+    return ninjaData.json();
 }
 
-const handleNames = (ninja) => {
-    const render = ninja.map(({name, id}) => `<li>${id}º ninja: ${name}</li>`).join('')
+const handleError = (error) => {
+    console.log(error.message)
+}
 
+const handleNinjas = ninjas => {
+    const listNinjas = ninjas.map(({name, id, images}) => ({
+        name,
+        id,
+        images
+    }));
+    // console.log(listNinjas[0].images[0]);
+    
+    handleCreateElement(listNinjas);
+}
+
+const handleCreateElement = (ninja) => {
+    const render = ninja.map(({name, id, images}) => 
+    `<li>${id}º ninja: ${name}</li>
+    <img class="card-image" src="${images[0]}"/> 
+    `).join('')
+    
     ninjaUl.innerHTML = render;
 }
 
 
-handleFetch();
+fetch(url)
+    .then(validate)
+    .then(handleNinjas)
+    .catch(handleError)
+    
